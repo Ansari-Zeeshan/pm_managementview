15fbab497c5a440fbe7a06c98ff9a7c5cec6af6d
Ansari <zansari765@gmail.com>
2022-02-17 17:26:15 +0530

    pushing

--- js/gantt.js
+++ js/gantt.js
@@ -1392,7 +1392,8 @@ function showinMobile1(multiplyTerm)
 }
 function showinMobile2(startTime, multiplyTerm)
 {
-    ganttTimeline[timelineIndex].style.cssText=`margin-left:${(startTime*9.8)+23.2}%; width:${multiplyTerm *9.67}%`;
+    console.log(startTime);
+    ganttTimeline[timelineIndex].style.cssText=`margin-left:${(startTime*9.67)+23.2}%; width:${multiplyTerm *9.67}%`;
 }
 function showinMobile3(endTimeline)
 {
